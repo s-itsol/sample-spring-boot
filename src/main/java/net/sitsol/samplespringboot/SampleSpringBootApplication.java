43259ae00eadeb89b5db80e5d738716ad1ebc8a2
rei.shibano <shibano@s-itsol.net>
2022-06-06 18:59:22 +0900

    logbackデフォルト推奨設定のxmlファイルを用意した。

--- src/main/java/net/sitsol/samplespringboot/SampleSpringBootApplication.java
+++ src/main/java/net/sitsol/samplespringboot/SampleSpringBootApplication.java
@@ -2,6 +2,7 @@ package net.sitsol.samplespringboot;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sitsol.samplespringboot.service.SampleService;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,8 +17,20 @@ public class SampleSpringBootApplication implements CommandLineRunner {
 	@Autowired
 	SampleService sampleService;
 
+	public static final String MDC_KEY_USER_ID = "userId";
+
 	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringBootApplication.class, args);
+		try {
+			// ログ用MDCキー値の設定
+			MDC.put(MDC_KEY_USER_ID, SampleSpringBootApplication.class.getSimpleName());
+
+			// spring-bootアプリケーション実行
+			SpringApplication.run(SampleSpringBootApplication.class, args);
+
+		} finally {
+			// ログ用MDCキー値の除去
+			MDC.remove(MDC_KEY_USER_ID);
+		}
 	}
 
 	@Override
@@ -29,6 +42,7 @@ public class SampleSpringBootApplication implements CommandLineRunner {
 		sampleService.execSample();
 
 		log.info("spring-bootコマンド処理本体-終了");
+
 	}
 
 }
